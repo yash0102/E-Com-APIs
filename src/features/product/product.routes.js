7835b670da47657405beb3c9853df9be25e604ae
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-10-13 21:14:51 +0530

    Add Product API

--- src/features/product/product.routes.js
+++ src/features/product/product.routes.js
@@ -1,6 +1,7 @@
 // 1. Import express.
 import express from 'express';
 import ProductController from './product.controller.js';
+import { upload } from '../../middlewares/fileupload.middleware.js';
 
 // 2. Initialize Express router.
 const router = express.Router();
@@ -9,6 +10,6 @@ const productController = new ProductController();
 
 // All the paths to controller methods(/api/products).
 router.get('/', productController.getAllProducts);
-router.post('/', productController.addProduct);
+router.post('/', upload.single('imageUrl'), productController.addProduct);
 
 export default router;
