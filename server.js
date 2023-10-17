d2d8bd3c250a5f9ac7056c277a79d84665ebce8a
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-10-17 18:29:12 +0530

    Basic Authentication

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import ProductRouter from './src/features/product/product.routes.js';
 import UserRouter from './src/features/user/user.routes.js';
+import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
 
 // 2. Create Server
 const server = express();
@@ -10,7 +11,7 @@ const server = express();
 server.use(bodyParser.json());
 
 // for all requests related to product , redirect to product routes.
-server.use('/api/products',ProductRouter); 
+server.use('/api/products',basicAuthorizer ,ProductRouter); 
 server.use('/api/users',UserRouter); 
 
 // 3. Default request handler
--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -4,13 +4,13 @@ export class UserController {
     signUp (req, res) {
         const { name, email, password, type } = req.body;
 
-        const user = UserModel.SignUp(name, email, password, type);
+        const user = UserModel.signUp(name, email, password, type);
 
         res.status(201).send(user);
     }
 
     signIn (req, res) {
-        const result = UserModel.SignIn(req.body.email , req.body.password);
+        const result = UserModel.signIn(req.body.email , req.body.password);
         if (!result){
             return res.status(400).send("Incorrect Credentials");
         } else {
