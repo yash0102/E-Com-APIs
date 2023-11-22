4d41e6e9a3dfb3accf17723e38f0a14d72689182
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-11-22 16:47:41 +0530

    Using Swagger

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // 1. Import Express
 import express from 'express';
+import swagger from 'swagger-ui-express';
 import bodyParser from 'body-parser';
+
 import ProductRouter from './src/features/product/product.routes.js';
 import UserRouter from './src/features/user/user.routes.js';
 import basicAuthorizer from './src/middlewares/basicAuth.middleware.js';
 import jwtAuth from './src/middlewares/jwt.middleware.js';
 import cartRouter from './src/features/cartItems/cartItems.routes.js';
+import apiDocs from './swagger.json' assert {type: 'json'};
 
 // 2. Create Server
 const server = express();
@@ -13,6 +16,7 @@ const server = express();
 server.use(bodyParser.json());
 
 // for all requests related to product , redirect to product routes.
+server.use('/api-docs', swagger.serve , swagger.setup(apiDocs)); 
 server.use('/api/products', jwtAuth ,ProductRouter); 
 server.use('/api/cartItems', jwtAuth ,cartRouter); 
 server.use('/api/users',UserRouter); 
