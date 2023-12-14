a1dd2b314e776dcc1b047458beb5fa63360d23b5
Yashwant Sonkar <yashsonkar0102@gmail.com>
2023-12-14 13:22:23 +0530

    Understanding Repository Pattern

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -1,34 +1,49 @@
 import UserModel from "./user.model.js";
 import jwt from 'jsonwebtoken';
+import UserRepository from "./user.repository.js";
 
 export class UserController {
-    async signUp (req, res) {
-        const { name, email, password, type } = req.body;
-
-        const user = await UserModel.signUp(name, email, password, type);
-
-        res.status(201).send(user);
+    constructor() {
+        this.userRepository = new UserRepository();
     }
 
-    signIn (req, res) {
-        const result = UserModel.signIn(req.body.email , req.body.password);
-        if (!result){
-            return res.status(400).send("Incorrect Credentials");
-        } else {
-            // 1. Create token
-            const token = jwt.sign(
-                { 
-                    userID: result.id,
-                    email: result.email,
-                }, 
-                "ifV5HcoOvGGIdoqHi4rX042w9GBBqWtj",
-                {
-                    expiresIn: '1h',
-                }
-            )
+    async signUp (req, res) {
+        try {
+            const { name, email, password, type } = req.body;
+            const user = new UserModel(name, email, password, type);
+            await this.userRepository.signUp(user);
+            res.status(201).send(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(400).send("Something went wrong");
+        }
+    }
 
-            // 2. send token
-            return res.status(200).send(token);
+    async signIn (req, res, next) {
+        try {
+            const result = await this.userRepository.signIn(req.body.email , req.body.password);
+            if (!result){
+                return res.status(400).send("Incorrect Credentials");
+            } else {
+                // 1. Create token
+                const token = jwt.sign(
+                    { 
+                        userID: result.id,
+                        email: result.email,
+                    }, 
+                    "ifV5HcoOvGGIdoqHi4rX042w9GBBqWtj",
+                    {
+                        expiresIn: '1h',
+                    }
+                )
+    
+                // 2. send token
+                return res.status(200).send(token);
+            }
+            
+        } catch (err) {
+            console.log(err);
+            return res.status(400).send("Something went wrong");
         }
     }
 }
--- src/features/user/user.routes.js
+++ src/features/user/user.routes.js
@@ -4,7 +4,11 @@ import { UserController } from "./user.controller.js";
 
 const userController = new UserController();
 
-router.post("/signup", userController.signUp);
-router.post("/signin", userController.signIn);
+router.post("/signup", (req, res) => {
+    userController.signUp(req, res);
+});
+router.post("/signin", (req, res)=> {
+    userController.signIn(req, res);
+});
 
 export default router;
