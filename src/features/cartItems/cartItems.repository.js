bf71756f371a99cd482f3887e3e89dd2c90e3dff
Yashwant Sonkar <yashsonkar0102@gmail.com>
2024-01-10 21:21:20 +0530

    Update Quantity

--- src/features/cartItems/cartItems.repository.js
+++ src/features/cartItems/cartItems.repository.js
@@ -11,7 +11,11 @@ export default class CartItemsRepository {
         try {
             const db = getDB();
             const collection = db.collection(this.collection);
-            await collection.insertOne({productID: new ObjectId(productID), userID: new ObjectId(userID), quantity});
+            await collection.updateOne(
+                { productID: new ObjectId(productID), userID: new ObjectId(userID)},
+                {$inc:{ quantity: quantity }},
+                { upsert: true}
+            );
         } catch (err) {
             console.log(err);
             throw new customErrorHandler("Something went wrong with database", 500);
