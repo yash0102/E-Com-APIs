ec2150af978690021f20e0cd31c52947154eb1a2
Yashwant Sonkar <yashsonkar0102@gmail.com>
2024-10-07 23:41:32 +0530

    Mongoose Middlewares

--- src/features/like/like.schema.js
+++ src/features/like/like.schema.js
@@ -14,4 +14,17 @@ export const likeSchema = new mongoose.Schema({
         type: String,
         enum: ['Product', 'Category']
     }
+}).pre('save', (next)=> {
+    console.log("New like coming in");
+    next();
+}).post('save', (doc)=> {
+    console.log("Like is saved");
+    console.log(doc);
+
+}).pre('find', (next)=> {
+    console.log("Retriving likes");
+    next();
+}).post('find', (docs)=> {
+    console.log("Find is completed");
+    console.log(docs);
 })
